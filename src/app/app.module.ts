7d27ece2a8295fc4ebebdbdb962ccf595ad86c5e
Jakub Kares (private) <jakub.kares@gmail.com>
2022-10-17 23:00:44 +0200

    Post component - one post in gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { PostContentComponent } from './post-content/post-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    PostContentComponent
   ],
   imports: [
     BrowserModule,
